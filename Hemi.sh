53a18cf5217f651af516ea0b8a14b09956a504e0
Andante <84418705+ksydoruk1508@users.noreply.github.com>
2024-11-27 13:32:59 +0200

    Update Hemi.sh

--- Hemi.sh
+++ Hemi.sh
@@ -244,7 +244,7 @@ function main_menu {
     while true; do
         echo -e "${YELLOW}Выберите действие:${NC}"
         echo -e "${CYAN}1. Установка ноды${NC}"
-        echo -e "${CYAN}2. Обновление ноды${NC}"
+        echo -e "${CYAN}2. Обновление ноды до версии 0.6.0 (27.11.2024)${NC}"
         echo -e "${CYAN}3. Рестарт ноды${NC}"
         echo -e "${CYAN}4. Изменить порт${NC}"
         echo -e "${CYAN}5. Изменить комиссию${NC}"
