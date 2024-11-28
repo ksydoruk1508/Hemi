a85bcf34367accc4073e560e56b95271c48a7798
Andante <84418705+ksydoruk1508@users.noreply.github.com>
2024-11-29 01:54:33 +0200

    Update Hemi.sh

--- Hemi.sh
+++ Hemi.sh
@@ -135,11 +135,11 @@ function update_node {
 
     # Установка бинарника
     echo -e "${BLUE}Загружаем бинарник Hemi...${NC}"
-    curl -L -O https://github.com/hemilabs/heminetwork/releases/download/v0.6.0/heminetwork_v0.6.0_linux_amd64.tar.gz
+    curl -L -O https://github.com/hemilabs/heminetwork/releases/download/v0.7.0/heminetwork_v0.7.0_linux_amd64.tar.gz
 
     # Создание директории и извлечение бинарника
     mkdir -p hemi
-    tar --strip-components=1 -xzvf heminetwork_v0.6.0_linux_amd64.tar.gz -C hemi
+    tar --strip-components=1 -xzvf heminetwork_v0.7.0_linux_amd64.tar.gz -C hemi
     cd hemi
 
     # Запрос приватного ключа и комиссии
@@ -257,7 +257,7 @@ function main_menu {
     while true; do
         echo -e "${YELLOW}Выберите действие:${NC}"
         echo -e "${CYAN}1. Установка ноды${NC}"
-        echo -e "${CYAN}2. Обновление ноды до версии 0.6.0 (27.11.2024)${NC}"
+        echo -e "${CYAN}2. Обновление ноды до версии 0.7.0 (29.11.2024)${NC}"
         echo -e "${CYAN}3. Рестарт ноды${NC}"
         echo -e "${CYAN}4. Изменить порт${NC}"
         echo -e "${CYAN}5. Изменить комиссию${NC}"
